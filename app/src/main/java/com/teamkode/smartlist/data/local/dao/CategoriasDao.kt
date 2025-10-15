982f3dfda0633b228638dd7f62167fb508ddf36b
Gil <gilmail3120@gmail.com>
2025-10-14 21:20:54 -0300

    feat:criacao categoriadao.

---
+++ app/src/main/java/com/teamkode/smartlist/data/local/dao/CategoriasDao.kt
@@ -0,0 +1,24 @@
+package com.teamkode.smartlist.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.teamkode.smartlist.domain.Categorias
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CategoriasDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCategoria(item: Categorias)
+
+    @Delete
+    suspend fun deleteCategoria(item: Categorias)
+
+    @Query("SELECT * FROM categoria")
+    fun getAllAnotacao(): Flow<List<Categorias>>
+
+    @Query("SELECT * FROM categoria WHERE id = :itemId")
+    suspend fun getAnotacaoById(itemId: Int): Categorias?
+}
