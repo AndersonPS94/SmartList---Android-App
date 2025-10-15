3c3ebc893c2c6f0cf1dcde346457a5e21190ff60
Gil <gilmail3120@gmail.com>
2025-10-14 21:13:46 -0300

    feat:criacao anotacaodao

---
+++ app/src/main/java/com/teamkode/smartlist/data/local/dao/AnoacaoDao.kt
@@ -0,0 +1,24 @@
+package com.teamkode.smartlist.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.teamkode.smartlist.domain.Anotacao
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AnoacaoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAnotacao(item: Anotacao)
+
+    @Delete
+    suspend fun deleteAnotacao(item: Anotacao)
+
+    @Query("SELECT * FROM anotacoes ORDER BY data DESC")
+    fun getAllAnotacao(): Flow<List<Anotacao>>
+
+    @Query("SELECT * FROM anotacoes WHERE id = :itemId")
+    suspend fun getAnotacaoById(itemId: Int): Anotacao?
+}
