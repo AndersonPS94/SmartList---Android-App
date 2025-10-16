c40843ff5a8fd7812760cc9e7df826956abaab03
Gil <gilmail3120@gmail.com>
2025-10-15 21:02:11 -0300

    feat:mudar nome de mapper.

--- app/src/main/java/com/teamkode/smartlist/data/mapper/AgendamentoCategoriaToAgendamentoDetalhada.kt
+++ app/src/main/java/com/teamkode/smartlist/data/mapper/AgendamentoCategoriaToAgendamentoDetalhada.kt
@@ -2,9 +2,6 @@ package com.teamkode.smartlist.data.mapper
 
 import com.teamkode.smartlist.data.local.relation.AnotacaoComCategoria
 import com.teamkode.smartlist.domain.AnotacaoDetalhada
-import com.teamkode.smartlist.domain.Categorias
-import java.time.LocalDateTime
-import kotlin.Long
 
 fun AnotacaoComCategoria.toAnotacaoDetalhada(): AnotacaoDetalhada {
     return AnotacaoDetalhada(
