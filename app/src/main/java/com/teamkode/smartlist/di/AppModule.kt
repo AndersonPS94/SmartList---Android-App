003e275eb0a69685cc4fecf38a051f35ac49f547
Gil <gilmail3120@gmail.com>
2025-11-07 00:31:22 -0300

    fix:excluido arquivos desnecessários e correcao do nome do banco.

---
+++ app/src/main/java/com/teamkode/smartlist/di/AppModule.kt
@@ -0,0 +1,24 @@
+package com.teamkode.smartlist.di
+
+import com.teamkode.smartlist.data.local.database.ShoppingListDatabase
+import com.teamkode.smartlist.data.repository.ShoppingItemRepositoryImpl
+import com.teamkode.smartlist.domain.repository.ShoppingItemRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideShoppingItemRepository(
+        db: ShoppingListDatabase
+    ) : ShoppingItemRepository {
+        return ShoppingItemRepositoryImpl(db.shoppingItemDao())
+
+    }
+}
